3541ee37699abe1ce0416af01a4f9397846e9ff9
Rameshwar Urdukhe <rpatil768093@gmail.com>
2026-06-22 08:41:02 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://sortmyscene-ticket-booking.onrender.com/",
 });
 
 export default api;
